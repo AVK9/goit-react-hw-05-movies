be24dc2ec5726a66d0d56d84f1c65b2175b2756e
AlexKat <katsubo79@gmail.com>
2023-12-20 14:42:55 +0200

    get

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,5 @@
+// import { getImages } from '../../../src/service/service';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* <getImages /> */}
     </div>
   );
 };
---
+++ src/service/service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTc2NzBmMzExOWVmNDc5ODFjMDljYmYxY2IzZDJkMSIsInN1YiI6IjY1ODJhNGVjNTViYzM1NTcxNTE3YjQ0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FGASUnS6pTqDNKCG13-tA5RbW9nYFMwY9-2qdwNpLWo'
+  }
+};
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+axios.defaults.params = {
+  include_adult: false,
+  language: 'en-US',
+};
+//----------------------------------------------------
+export const getTrending = async () => {
+  const { data } = await axios.get('trending/all/day', options)
+  console.log(data);
+  return data;
+};
+// getTrending()
+//----------------------------------------------------
+export const searchMovies = async (query, page) => {
+  const { data } = await axios.get(`/search/movie?query=${query}&page=${page}`, options)
+  console.log(data);
+  return data;
+};
+// searchMovies('Cat', 3)
+//----------------------------------------------------
+export const getMovieDetails = async (movie_id) => {
+  const { data } = await axios.get(`/movie/${movie_id}`, options)
+  console.log(data);
+  return data;
+};
+// getMovieDetails(695721)
+//----------------------------------------------------
+export const getMovieCredits = async (movie_id, page) => {
+  const { data } = await axios.get(`/movie/${movie_id}/reviews?page=${page}`, options)
+  console.log(data);
+  return data;
+};
+// getMovieCredits(695721, 1)
