4060af14f19fe9faaa50345b5cee528df190aa5f
AlexKat <katsubo79@gmail.com>
2023-12-28 15:14:34 +0200

    input

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,22 +4,30 @@ import { Button, InpuBox, Input, Form } from './SearchForm.module';
 
 const SearchForm = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [inputSearch, setInputSearch] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
+  const reset = () => {
+    setInputSearch('');
+  };
+  if (inputSearch === null) {
+    reset();
+  }
+  const handleChange = ({ target: { value } }) => {
+    setSearchQuery(value);
+    setInputSearch(value);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
     // submit(searchQuery);
     setSearchParams({ search: searchQuery });
+    reset();
   };
 
-  const handleChange = ({ target: { value } }) => {
-    setSearchQuery(value);
-  };
   useEffect(() => {
     !searchQuery && setSearchQuery(searchParams.get('search'));
   }, [searchParams, searchQuery]);
-  if (searchQuery === null) {
-    setSearchQuery('');
-  }
+
   return (
     <InpuBox>
       <Form onSubmit={handleSubmit}>
@@ -28,7 +36,7 @@ const SearchForm = () => {
           type="text"
           id="exInputSearch"
           onChange={handleChange}
-          value={searchQuery}
+          value={inputSearch}
         />
         <Button type="submit">Search</Button>
       </Form>
